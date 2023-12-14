809466222b3d7a58c666d605fae89e84d35120f0
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2023-12-14 21:35:51 +0500

    All done

---
+++ index.js
@@ -0,0 +1,122 @@
+    // 1-masala
+// let n = +prompt(`Sonni kiriting...`);
+// let counter = 0;
+
+// for (let i = 0; i < n; i++) {
+//     if (i % 3 == 0 && i % 7 == 0) {
+//         counter++;
+//     }   
+// }
+
+// console.log(counter);
+
+
+
+    // 2-masala
+// let n = +prompt(`Sonni kiriting...`);
+// let sum = 0;
+
+// for (let i = n; i >= 1; i--) {
+//     if (i % 2 == 0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
+
+
+
+    // 3-masala
+// let n = +prompt('Sonni kiriting');
+// let sum = 0;
+
+// for (let i = 0; i <= n; i++) {
+//     if (i % 5 && i % 7 != 0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
+
+
+
+    // 4-masala
+// let a = +prompt(`Birinchi sonni kirting...`);
+// let b = +prompt(`Ikkinchi sonni kirting...`);
+// let sum = 0;
+
+// for (let i = a; i <= b; i++) {
+//     if (i < 0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
+
+
+
+    // 5-masala
+// let n = +prompt(`Sonni kiriting...`);
+// let res = 1;
+
+// for (let i = 1; i <= n; i++) {
+//     res *= i;
+// }
+
+// console.log(res);
+
+
+
+    // 6-masala
+// let y = +prompt('Sonni kiriting...');
+
+// for (let i = 0; i <= y; i++) {
+//     if (y % i == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+    // 7-masala
+// let x = +prompt('Sonni kiriting...');
+// let counter = 0;
+
+// for (let i = 0; i <= x; i++) {
+//     if (x % i == 0) {
+//         counter++;
+//     }
+// }
+
+// console.log(counter);
+
+
+
+    // 8-masala
+// let n = +prompt('Sonni kiriting...');
+// let counter = 0;
+
+// for (let i = 0; i <= n; i++) {
+//     if (n % i == 0) {
+//         counter++;
+//     }
+// }
+
+// if (counter == 2) {
+//     console.log(`Tub son`);
+// } else {
+//     console.log('Tub son emas');
+// }
+
+
+
+    // 9-masala
+// let n = +prompt('Sonni kiriting...');
+// let sum = 0;
+
+// for (let i = 0; i <= n; i++) {
+//     sum = sum + (i ** 2);
+// }
+
+// console.log(sum);
+
